d0c7bf16de60ed900da5c276a1ae8c8b8e613f0f
Tanuja Pilane <tanujapilane8114@gmail.com>
2025-03-02 13:17:53 +0530

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -2,15 +2,11 @@ import sqlite3
 
 conn = sqlite3.connect("jobs.db")
 cursor = conn.cursor()
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS jobs (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    job_title TEXT NOT NULL,
-    company TEXT NOT NULL,
-    location TEXT NOT NULL
-)
-""")
-
+cursor.execute('''CREATE TABLE IF NOT EXISTS jobs (
+                     id INTEGER PRIMARY KEY AUTOINCREMENT,
+                     job_title TEXT NOT NULL,
+                     company TEXT NOT NULL,
+                     location TEXT NOT NULL)''')
 conn.commit()
 conn.close()
+print("Database setup complete.")
